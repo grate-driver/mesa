c503cacb531a57df3ad1dd372334e920e327e1ed
Brian Paul <brianp@vmware.com>
2009-02-21 14:53:25 -0700

    mesa: use an array for current texture objects
    Use loops to consolidate lots of texture object code.
    (cherry picked from commit 9818734e0148510967ca9ee0d1aa8b196b509f02)

--- src/mesa/swrast/s_triangle.c
+++ src/mesa/swrast/s_triangle.c
@@ -131,7 +131,8 @@ _swrast_culltriangle( GLcontext *ctx,
 
 #define SETUP_CODE							\
    struct gl_renderbuffer *rb = ctx->DrawBuffer->_ColorDrawBuffers[0];	\
-   struct gl_texture_object *obj = ctx->Texture.Unit[0].Current2D;	\
+   struct gl_texture_object *obj = 					\
+      ctx->Texture.Unit[0].CurrentTex[TEXTURE_2D_INDEX];		\
    const GLint b = obj->BaseLevel;					\
    const GLfloat twidth = (GLfloat) obj->Image[0][b]->Width;		\
    const GLfloat theight = (GLfloat) obj->Image[0][b]->Height;		\
@@ -182,7 +183,8 @@ _swrast_culltriangle( GLcontext *ctx,
 
 #define SETUP_CODE							\
    struct gl_renderbuffer *rb = ctx->DrawBuffer->_ColorDrawBuffers[0];	\
-   struct gl_texture_object *obj = ctx->Texture.Unit[0].Current2D;	\
+   struct gl_texture_object *obj = 					\
+      ctx->Texture.Unit[0].CurrentTex[TEXTURE_2D_INDEX];		\
    const GLint b = obj->BaseLevel;					\
    const GLfloat twidth = (GLfloat) obj->Image[0][b]->Width;		\
    const GLfloat theight = (GLfloat) obj->Image[0][b]->Height;		\
@@ -523,7 +525,8 @@ affine_span(GLcontext *ctx, SWspan *span,
 #define SETUP_CODE							\
    struct affine_info info;						\
    struct gl_texture_unit *unit = ctx->Texture.Unit+0;			\
-   struct gl_texture_object *obj = unit->Current2D;			\
+   struct gl_texture_object *obj = 					\
+      ctx->Texture.Unit[0].CurrentTex[TEXTURE_2D_INDEX];		\
    const GLint b = obj->BaseLevel;					\
    const GLfloat twidth = (GLfloat) obj->Image[0][b]->Width;		\
    const GLfloat theight = (GLfloat) obj->Image[0][b]->Height;		\
@@ -793,7 +796,8 @@ fast_persp_span(GLcontext *ctx, SWspan *span,
 #define SETUP_CODE							\
    struct persp_info info;						\
    const struct gl_texture_unit *unit = ctx->Texture.Unit+0;		\
-   const struct gl_texture_object *obj = unit->Current2D;		\
+   struct gl_texture_object *obj = 					\
+      ctx->Texture.Unit[0].CurrentTex[TEXTURE_2D_INDEX];		\
    const GLint b = obj->BaseLevel;					\
    info.texture = (const GLchan *) obj->Image[0][b]->Data;		\
    info.twidth_log2 = obj->Image[0][b]->WidthLog2;			\
@@ -1049,7 +1053,8 @@ _swrast_choose_triangle( GLcontext *ctx )
          const struct gl_texture_image *texImg;
          GLenum minFilter, magFilter, envMode;
          GLint format;
-         texObj2D = ctx->Texture.Unit[0].Current2D;
+         texObj2D = ctx->Texture.Unit[0].CurrentTex[TEXTURE_2D_INDEX];
+
          texImg = texObj2D ? texObj2D->Image[0][texObj2D->BaseLevel] : NULL;
          format = texImg ? texImg->TexFormat->MesaFormat : -1;
          minFilter = texObj2D ? texObj2D->MinFilter : (GLenum) 0;
