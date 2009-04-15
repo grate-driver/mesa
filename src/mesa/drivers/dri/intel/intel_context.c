85396d8ef4f7fc4702578a3d4e89844c7e238acc
Ian Romanick <ian.d.romanick@intel.com>
2009-04-15 15:12:48 -0700

    intel: Bump driver date

--- src/mesa/drivers/dri/intel/intel_context.c
+++ src/mesa/drivers/dri/intel/intel_context.c
@@ -97,7 +97,7 @@ int INTEL_DEBUG = (0);
 
 #include "extension_helper.h"
 
-#define DRIVER_DATE                     "20090326 2009Q1 RC2"
+#define DRIVER_DATE                     "20090415 2009Q1 RC3"
 #define DRIVER_DATE_GEM                 "GEM " DRIVER_DATE
 
 static const GLubyte *
