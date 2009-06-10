02659868e888f8575ed4a3f6f0d269a7bbe2c5c2
Brian Paul <brianp@vmware.com>
2009-06-10 12:26:12 -0600

    glsl: fix warnings, update comments, s/TRUE/GL_TRUE/
    (cherry picked from master, commit 7fdd64ab29576e607434fb8c82ddfa61e8ea6aa8)

--- src/mesa/shader/slang/slang_preprocess.c
+++ src/mesa/shader/slang/slang_preprocess.c
@@ -1306,14 +1306,15 @@ error:
 }
 
 
-/*
+/**
  * Remove the continuation characters from the input string.
  * This is the very first step in preprocessing and is effective
  * even inside comment blocks.
  * If there is a whitespace between a backslash and a newline,
  * this is not considered as a line continuation.
+ * \return GL_TRUE for success, GL_FALSE otherwise.
  */
-GLboolean
+static GLboolean
 _slang_preprocess_backslashes(slang_string *output,
                               const char *input)
 {
@@ -1340,7 +1341,7 @@ _slang_preprocess_backslashes(slang_string *output,
          slang_string_pushc(output, *input++);
       }
    }
-   return TRUE;
+   return GL_TRUE;
 }
 
 
