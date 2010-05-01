aras-p/glsl-optimizer

commit e42d84eaba228d4d96a46d116c6ca24581e29439

glx: Fix build

src/glx/glxcmds.c
@@ -887,7 +887,7 @@ glXCopyContext(Display * dpy, GLXContext source,
 static Bool
 __glXIsDirect(Display * dpy, GLXContextID contextID)
 {
-#ifndef GLX_USE_APPLEGL /* TODO: apple indirect */
+#ifdef GLX_USE_APPLEGL /* TODO: apple indirect */
    return GC_IS_DIRECT(gc);
 #else
 #if !defined(USE_XCB)

grate-driver/mesa

commit 4d63be67f320839d115987211d0d9b4297f6d17d

darwin: Fix build
This is a regression from e42d84eaba228d4d96a46d116c6ca24581e29439

https://bugs.freedesktop.org/show_bug.cgi?id=27929

Signed-off-by: Jeremy Huddleston <jeremyhu@apple.com>

src/glx/glxcmds.c
@@ -887,9 +887,6 @@ glXCopyContext(Display * dpy, GLXContext source,
 static Bool
 __glXIsDirect(Display * dpy, GLXContextID contextID)
 {
-#ifdef GLX_USE_APPLEGL /* TODO: apple indirect */
-   return GC_IS_DIRECT(gc);
-#else
 #if !defined(USE_XCB)
    xGLXIsDirectReq *req;
    xGLXIsDirectReply reply;
@@ -925,7 +922,6 @@ __glXIsDirect(Display * dpy, GLXContextID contextID)
 
    return reply.isDirect;
 #endif /* USE_XCB */
-#endif /* GLX_USE_APPLEGL */
 }
 
 /**
@@ -943,7 +939,11 @@ glXIsDirect(Display * dpy, GLXContext gc)
    else if (GC_IS_DIRECT(gc)) {
       return GL_TRUE;
    }
+#ifdef GLX_USE_APPLEGL  /* TODO: indirect on darwin */
+      return GL_FALSE;
+#else
    return __glXIsDirect(dpy, gc->xid);
+#endif
 }
 
 PUBLIC GLXPixmap