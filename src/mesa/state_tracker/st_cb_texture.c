7ffeced927e22965145b907c1941cdead6c9615b
Henri Verbeet <hverbeet@gmail.com>
2010-12-22 08:28:39 +0100

    st/mesa: Handle wrapped depth buffers in st_copy_texsubimage(). (cherry picked from commit 59051ad44310d7f744fcb0706326564990b554dd)

--- src/mesa/state_tracker/st_cb_texture.c
+++ src/mesa/state_tracker/st_cb_texture.c
@@ -1523,6 +1523,9 @@ st_copy_texsubimage(GLcontext *ctx,
    if (texBaseFormat == GL_DEPTH_COMPONENT ||
        texBaseFormat == GL_DEPTH_STENCIL) {
       strb = st_renderbuffer(fb->_DepthBuffer);
+      if (strb->Base.Wrapped) {
+         strb = st_renderbuffer(strb->Base.Wrapped);
+      }
    }
    else {
       /* texBaseFormat == GL_RGB, GL_RGBA, GL_ALPHA, etc */
