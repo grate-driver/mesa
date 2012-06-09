cb70af5c9404138d292d8490e35f61504358de40
Kenneth Graunke <kenneth@whitecape.org>
2012-06-08 22:12:23 -0700

    mesa: Check for a negative "size" parameter in glCopyBufferSubData().
    From the GL_ARB_copy_buffer spec:
    "An INVALID_VALUE error is generated if any of readoffset, writeoffset,
     or size are negative [...]"
    
    Fixes oglconform's copybuffer/negative.CNNegativeValues test.
    
    NOTE: This is a candidate for stable release branches.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    (cherry picked from commit a75e704326986b85539b6abf2319701e1a3138e9)

--- src/mesa/main/bufferobj.c
+++ src/mesa/main/bufferobj.c
@@ -1311,6 +1311,12 @@ _mesa_CopyBufferSubData(GLenum readTarget, GLenum writeTarget,
       return;
    }
 
+   if (size < 0) {
+      _mesa_error(ctx, GL_INVALID_VALUE,
+                  "glCopyBufferSubData(writeOffset = %d)", (int) size);
+      return;
+   }
+
    if (readOffset + size > src->Size) {
       _mesa_error(ctx, GL_INVALID_VALUE,
                   "glCopyBuffserSubData(readOffset + size = %d)",
