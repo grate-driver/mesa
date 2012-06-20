9ca7b5b65e53c09d04ab96351b39dc202f494ef2
Paul Berry <stereotype441@gmail.com>
2012-06-20 12:31:46 -0700

    mesa: Add UsesDFdy to struct gl_fragment_program.
    The i965 back-end needs to compile dFdy() differently for FBOs and
    window system framebuffers, because Y coordinates are flipped between
    the two (see commit 82d2596: i965: Compute dFdy() correctly for FBOs).
    This boolean will allow it to avoid unnecessarily recompiling shaders
    that don't use dFdy().
    
    Reviewed-by: Brian Paul <brianp@vmware.com>
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    (cherry picked from commit 5e310e9f8300a357d6bdaf098c72098518b564f3)

--- src/mesa/program/program.c
+++ src/mesa/program/program.c
@@ -551,6 +551,7 @@ _mesa_clone_program(struct gl_context *ctx, const struct gl_program *prog)
             = (const struct gl_fragment_program *) prog;
          struct gl_fragment_program *fpc = (struct gl_fragment_program *) clone;
          fpc->UsesKill = fp->UsesKill;
+         fpc->UsesDFdy = fp->UsesDFdy;
          fpc->OriginUpperLeft = fp->OriginUpperLeft;
          fpc->PixelCenterInteger = fp->PixelCenterInteger;
       }
@@ -772,6 +773,7 @@ _mesa_combine_programs(struct gl_context *ctx,
       newFprog = (struct gl_fragment_program *) newProg;
 
       newFprog->UsesKill = fprogA->UsesKill || fprogB->UsesKill;
+      newFprog->UsesDFdy = fprogA->UsesDFdy || fprogB->UsesDFdy;
 
       /* We'll do a search and replace for instances
        * of progB_colorFile/progB_colorIndex below...
