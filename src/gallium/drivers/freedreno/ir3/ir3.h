1dec8bbb8e2971ac0cd55dee279ce10ae22242f1
Rob Clark <robclark@freedesktop.org>
2015-02-26 15:13:10 -0500

    freedreno/ir3: add support for memory (cat6) instructions
    Scheduled basically the same as texture (cat5) instructions, using (sy)
    flag for synchronization.
    
    Signed-off-by: Rob Clark <robclark@freedesktop.org>
    (cherry picked from commit 9d732d3125e1b39788a642a5723aeb54cb1983f3)

--- src/gallium/drivers/freedreno/ir3/ir3.h
+++ src/gallium/drivers/freedreno/ir3/ir3.h
@@ -420,6 +420,11 @@ static inline bool is_tex(struct ir3_instruction *instr)
 	return (instr->category == 5);
 }
 
+static inline bool is_mem(struct ir3_instruction *instr)
+{
+	return (instr->category == 6);
+}
+
 static inline bool is_input(struct ir3_instruction *instr)
 {
 	return (instr->category == 2) && (instr->opc == OPC_BARY_F);
