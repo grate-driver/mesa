f9325a97b31fdeca74f861315a9654e006d222d5
Marek Olšák <marek.olsak@amd.com>
2015-11-05 23:56:38 +0100

    radeonsi: initialize SX_PS_DOWNCONVERT to 0 on Stoney
    otherwise the SX or CB blocks can go bananas
    
    Reviewed-by: Nicolai Hähnle <nicolai.haehnle@amd.com>
    Cc: mesa-stable@lists.freedesktop.org
    (cherry picked from commit 40912dd91e96376517fb41bb4dc228b45fd1a01c)
    [Emil Velikov: resolve trivial conflicts]
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    
    Conflicts:
    	src/gallium/drivers/radeonsi/si_state.c

--- src/gallium/drivers/radeonsi/si_state.c
+++ src/gallium/drivers/radeonsi/si_state.c
@@ -3342,5 +3342,8 @@ static void si_init_config(struct si_context *sctx)
 		si_pm4_set_reg(pm4, R_028C5C_VGT_OUT_DEALLOC_CNTL, 32);
 	}
 
+	if (sctx->b.family == CHIP_STONEY)
+		si_pm4_set_reg(pm4, R_028754_SX_PS_DOWNCONVERT, 0);
+
 	sctx->init_config = pm4;
 }
