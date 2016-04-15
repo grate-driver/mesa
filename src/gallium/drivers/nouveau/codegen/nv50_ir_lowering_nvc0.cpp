grate-driver/mesa

commit a785cccc706feeaba16e3df0b41bc4548c9be2b4

nvc0/ir: fix picking of coordinates from tex instruction for textureGrad
On Fermi, there's an argument in front of the coords that combines array
and indirect handle, while on Kepler the array and the indirect handle
are separate (and in front of the coords). We were previously only
accounting for the array bit of it, if there were an indirect access it
wouldn't be counted in the formula.

Signed-off-by: Ilia Mirkin <imirkin@alum.mit.edu>
Cc: "11.1 11.2" <mesa-stable@lists.freedesktop.org>
(cherry picked from commit f667d15561820ee9dd8e836d43cce3ee52a4780e)
[Emil Velikov: resolve trivial conflicts]
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

Conflicts:
    src/gallium/drivers/nouveau/codegen/nv50_ir_lowering_nvc0.cpp

src/gallium/drivers/nouveau/codegen/nv50_ir_lowering_nvc0.cpp
@@ -852,6 +852,17 @@ NVC0LoweringPass::handleManualTXD(TexInstruction *i)
    const int dim = i->tex.target.getDim();
    const int array = i->tex.target.isArray();
 
+   // This function is invoked after handleTEX lowering, so we have to expect
+   // the arguments in the order that the hw wants them. For Fermi, array and
+   // indirect are both in the leading arg, while for Kepler, array and
+   // indirect are separate (and both precede the coordinates). Maxwell is
+   // handled in a separate function.
+   unsigned array;
+   if (targ->getChipset() < NVISA_GK104_CHIPSET)
+      array = i->tex.target.isArray() || i->tex.rIndirectSrc >= 0;
+   else
+      array = i->tex.target.isArray() + (i->tex.rIndirectSrc >= 0);
+
    i->op = OP_TEX; // no need to clone dPdx/dPdy later
 
    for (c = 0; c < dim; ++c)

commit fd1e4331ee40627e5a8cad67b930bde4732ef93f

nvc/ir: remove duplicate variable declaration
Missed out with earlier commit while resolving conflicts.

Fixes: a785cccc706 "nvc0/ir: fix picking of coordinates from tex
instruction for textureGrad"
Reported-by: Ilia Mirkin <imirkin@alum.mit.edu>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/gallium/drivers/nouveau/codegen/nv50_ir_lowering_nvc0.cpp
@@ -850,7 +850,6 @@ NVC0LoweringPass::handleManualTXD(TexInstruction *i)
    Value *zero = bld.loadImm(bld.getSSA(), 0);
    int l, c;
    const int dim = i->tex.target.getDim();
-   const int array = i->tex.target.isArray();
 
    // This function is invoked after handleTEX lowering, so we have to expect
    // the arguments in the order that the hw wants them. For Fermi, array and