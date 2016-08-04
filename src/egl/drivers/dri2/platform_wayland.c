6ec0c92b3c026ade1bcbaa54a47f8dc2b5c772fa
Nicolas Boichat <drinkcat@chromium.org>
2016-08-04 10:07:51 +0800

    egl/wayland: Set disp->DriverData to NULL on error
    Avoid use-after-free, fix spec@egl_khr_fence_sync@conformance.
    
    Fixes: 9ee683f877 (egl/dri2: Add reference count for dri2_egl_display)
    Cc: "12.0" <mesa-stable@lists.freedesktop.org>
    Reported-by: Michel Dänzer <michel@daenzer.net>
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Martin Peres <martin.peres@linux.intel.com>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>
    (cherry picked from commit 48fd952f28a5fcd71eed5a60c8e3a10231c7a5b0)

--- src/egl/drivers/dri2/platform_wayland.c
+++ src/egl/drivers/dri2/platform_wayland.c
@@ -1238,6 +1238,7 @@ dri2_initialize_wayland_drm(_EGLDriver *drv, _EGLDisplay *disp)
    wl_event_queue_destroy(dri2_dpy->wl_queue);
  cleanup_dpy:
    free(dri2_dpy);
+   disp->DriverData = NULL;
 
    return EGL_FALSE;
 }
@@ -1883,6 +1884,7 @@ dri2_initialize_wayland_swrast(_EGLDriver *drv, _EGLDisplay *disp)
    wl_event_queue_destroy(dri2_dpy->wl_queue);
  cleanup_dpy:
    free(dri2_dpy);
+   disp->DriverData = NULL;
 
    return EGL_FALSE;
 }
