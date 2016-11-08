openharmony/third_party_mesa3d

anv/wsi: fix apps that acquire multiple images up front
This fix was found in the radv codebase when running dota2,
no idea if anyone has reported it on anv, but the same problem
occurs.

Once an image is acquired we need to mark it busy.

Acked-by: Edward O'Callaghan <funfunctor@folklore1984.net>
Cc: "12.0" <mesa-stable@lists.freedesktop.org>
Signed-off-by: Dave Airlie <airlied@redhat.com>

src/intel/vulkan/anv_wsi_wayland.c
@@ -523,6 +523,7 @@ wsi_wl_swapchain_acquire_next_image(struct anv_swapchain *anv_chain,
          if (!chain->images[i].busy) {
             /* We found a non-busy image */
             *image_index = i;
+            chain->images[image_index].busy = true;
             return VK_SUCCESS;
          }
       }

grate-driver/mesa

(cherry picked from commit 8980ac041167fb5a80be76e2f912b96117afa2cf)
Squashed with commit
anv: fix the wayland wsi busy flag setting
(cherry picked from commit a3834ebaf9ea8ff6826651392978cdb73b7fab96)

src/intel/vulkan/anv_wsi_wayland.c
@@ -519,6 +519,7 @@ wsi_wl_swapchain_acquire_next_image(struct anv_swapchain *anv_chain,
          if (!chain->images[i].busy) {
             /* We found a non-busy image */
             *image_index = i;
+            chain->images[i].busy = true;
             return VK_SUCCESS;
          }
       }