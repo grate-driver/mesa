yuq/mesa-lima

commit 373f118c6c750d717fd0727fc3fc191828714c6f

gallium: do not wrap header inclusion in
Add one missing extern C guard within include/pipe/p_video_enums.h, and
remove the wrapping throughout gallium.

On Haiku one could even use the gallium debug_printf() although
that's another topic.

v2: Leave dbghelp.h as is (Jose)

Cc: Jose Fonseca <jfonseca@vmware.com>
Cc: Brian Paul <brianp@vmware.com>
Cc: Alexander von Gluck IV <kallisti5@unixzen.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
Reviewed-by: Jose Fonseca <jfonseca@vmware.com>

src/gallium/auxiliary/util/u_debug.h
@@ -39,6 +39,11 @@
 #define U_DEBUG_H_
 
 
+#if defined(PIPE_OS_HAIKU)
+/* Haiku provides debug_printf in libroot with OS.h */
+#include <OS.h>
+#endif
+
 #include "os/os_misc.h"
 
 #include "pipe/p_format.h"
@@ -94,9 +99,6 @@ debug_printf(const char *format, ...)
    (void) format; /* silence warning */
 #endif
 }
-#else /* is Haiku */
-/* Haiku provides debug_printf in libroot with OS.h */
-#include <OS.h>
 #endif
 
 

src/gallium/auxiliary/tgsi/tgsi_sanity.h
@@ -28,11 +28,11 @@
 #ifndef TGSI_SANITY_H
 #define TGSI_SANITY_H
 
+#include "pipe/p_compiler.h"
+
 #if defined __cplusplus
 extern "C" {
 #endif
-
-#include "pipe/p_compiler.h"
 
 struct tgsi_token;
 

src/gallium/auxiliary/tgsi/tgsi_text.h
@@ -28,11 +28,11 @@
 #ifndef TGSI_TEXT_H
 #define TGSI_TEXT_H
 
+#include "pipe/p_compiler.h"
+
 #if defined __cplusplus
 extern "C" {
 #endif
-
-#include "pipe/p_compiler.h"
 
 struct tgsi_token;
 

src/gallium/auxiliary/util/u_draw_quad.h
@@ -32,6 +32,7 @@
 #include "pipe/p_compiler.h"
 #include "pipe/p_context.h"
 
+#include "util/u_draw.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -39,8 +40,6 @@ extern "C" {
 
 struct pipe_resource;
 struct cso_context;
-
-#include "util/u_draw.h"
 
 extern void 
 util_draw_vertex_buffer(struct pipe_context *pipe, struct cso_context *cso,

src/gallium/auxiliary/util/u_helpers.h
@@ -28,11 +28,11 @@
 #ifndef U_HELPERS_H
 #define U_HELPERS_H
 
+#include "pipe/p_state.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "pipe/p_state.h"
 
 void util_set_vertex_buffers_mask(struct pipe_vertex_buffer *dst,
                                   uint32_t *enabled_buffers,

src/gallium/auxiliary/util/u_video.h
@@ -28,10 +28,6 @@
 #ifndef U_VIDEO_H
 #define U_VIDEO_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "pipe/p_defines.h"
 #include "pipe/p_video_enums.h"
 
@@ -39,6 +35,10 @@ extern "C" {
 #include "pipe/p_compiler.h"
 #include "util/u_debug.h"
 #include "util/u_math.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 static inline enum pipe_video_format
 u_reduce_video_profile(enum pipe_video_profile profile)

src/gallium/include/pipe/p_format.h
@@ -29,11 +29,11 @@
 #ifndef PIPE_FORMAT_H
 #define PIPE_FORMAT_H
 
+#include "p_config.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "p_config.h"
 
 /**
  * Formats for textures, surfaces and vertex data

src/gallium/include/pipe/p_video_codec.h
@@ -28,11 +28,11 @@
 #ifndef PIPE_VIDEO_CONTEXT_H
 #define PIPE_VIDEO_CONTEXT_H
 
+#include "pipe/p_video_state.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#include "pipe/p_video_state.h"
 
 struct pipe_screen;
 struct pipe_surface;

grate-driver/mesa

commit c1c39334dac70cbb5bce2a274e538cd2977023a7

haiku: build fixes around debug defines
Move the os/os_misc.h include further up, since it's the one that
implicitly provides the PIPE_OS_HAIKU define.

(cherry picked from commit 40b0c8666c337fd0fdff42ce70703cd300abcf0c)
Fixes: 373f118c6c7 ("gallium: do not wrap header inclusion in")
Nominated-by: Emil Velikov <emil.velikov@collabora.com>
[Emil Velikov: add commit message, fixes tag]
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/gallium/auxiliary/util/u_debug.h
@@ -39,12 +39,12 @@
 #define U_DEBUG_H_
 
 
+#include "os/os_misc.h"
+
 #if defined(PIPE_OS_HAIKU)
 /* Haiku provides debug_printf in libroot with OS.h */
 #include <OS.h>
 #endif
-
-#include "os/os_misc.h"
 
 #include "pipe/p_format.h"
 #include "pipe/p_defines.h"