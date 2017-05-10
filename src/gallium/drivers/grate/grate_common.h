2bb08322973a13225f94a28cc16e6b0a072c31f4
Erik Faye-Lund <kusmabite@gmail.com>
2017-05-10 23:07:36 +0000

    grate: prepare for draw

--- src/gallium/drivers/grate/grate_common.h
+++ src/gallium/drivers/grate/grate_common.h
@@ -8,4 +8,11 @@
       printf("TODO: %s()\n", __func__); \
 } while (0)
 
+#define TGR3D_VAL(reg_name, field_name, value) \
+   (((value) << TGR3D_ ## reg_name ## _ ## field_name ## __SHIFT) & \
+           TGR3D_ ## reg_name ## _ ## field_name ## __MASK)
+
+#define TGR3D_BOOL(reg_name, field_name, bool) \
+   ((bool) ? TGR3D_ ## reg_name ## _ ## field_name : 0)
+
 #endif
