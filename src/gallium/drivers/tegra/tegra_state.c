9eafd8aadad4cba29087ae6b2db8521959be669f
Erik Faye-Lund <kusmabite@gmail.com>
2017-05-13 16:29:39 +0000

    print when creating the vs-state

--- src/gallium/drivers/tegra/tegra_state.c
+++ src/gallium/drivers/tegra/tegra_state.c
@@ -346,37 +346,35 @@ static void *
 tegra_create_vertex_state(struct pipe_context *pcontext, unsigned int count,
 			  const struct pipe_vertex_element *elements)
 {
+	unsigned int i;
 	struct tegra_vertex_state *vtx = CALLOC_STRUCT(tegra_vertex_state);
 	if (!vtx)
 		return NULL;
 
+	fprintf(stdout, "> %s(pcontext=%p, count=%d, elements=%p)\n", __func__, pcontext, count, elements);
+
+	fprintf(stdout, "  elements:\n");
+	for (i = 0; i < count; ++i) {
+		const struct pipe_vertex_element *e = &elements[i];
+
+		fprintf(stdout, "    %u:\n", i);
+		fprintf(stdout, "      src_offset: %u\n", e->src_offset);
+		fprintf(stdout, "      instance_divisor: %u\n", e->instance_divisor);
+		fprintf(stdout, "      vertex_buffer_index: %u\n", e->vertex_buffer_index);
+		fprintf(stdout, "      src_format: %d\n", e->src_format);
+	}
+
 	memcpy(vtx->elements, elements, sizeof(*elements) * count);
 	vtx->num_elements = count;
+
+	fprintf(stdout, "< %s()\n", __func__);
 
 	return vtx;
 }
 
 static void tegra_bind_vertex_state(struct pipe_context *pcontext, void *so)
 {
-	struct tegra_vertex_state *vs = so;
-	unsigned int i;
-
-	fprintf(stdout, "> %s(pcontext=%p, so=%p)\n", __func__, pcontext, so);
-	if (vs) {
-		fprintf(stdout, "  vs:\n");
-		fprintf(stdout, "    num_elements: %u\n", vs->num_elements);
-
-		for (i = 0; i < vs->num_elements; i++) {
-			struct pipe_vertex_element *element = &vs->elements[i];
-
-			fprintf(stdout, "      %u:\n", i);
-			fprintf(stdout, "        src_offset: %u\n", element->src_offset);
-			fprintf(stdout, "        instance_divisor: %u\n", element->instance_divisor);
-			fprintf(stdout, "        vertex_buffer_index: %u\n", element->vertex_buffer_index);
-			fprintf(stdout, "        src_format: %d\n", element->src_format);
-		}
-	}
-	fprintf(stdout, "< %s()\n", __func__);
+	unimplemented();
 }
 
 static void tegra_delete_vertex_state(struct pipe_context *pcontext, void *so)
