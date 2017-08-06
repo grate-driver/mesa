508f2dd7ade4466ef6b4cd484f15722b5e2b3c53
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-06 22:58:06 +0200

    grate: implement 2D functionality
    This is sufficient for clears and readpixels etc.

--- src/gallium/drivers/grate/grate_stream.h
+++ src/gallium/drivers/grate/grate_stream.h
@@ -25,37 +25,37 @@
  *	Arto Merilainen <amerilainen@nvidia.com>
  */
 
-#ifndef TEGRA_STREAM_H_
-#define TEGRA_STREAM_H_
+#ifndef GRATE_STREAM_H_
+#define GRATE_STREAM_H_
 
 #include <stdint.h>
 #include <libdrm/tegra.h>
 
 #include "class_ids.h"
 
-enum tegra_stream_status {
-   TEGRADRM_STREAM_FREE,
-   TEGRADRM_STREAM_CONSTRUCT,
-   TEGRADRM_STREAM_CONSTRUCTION_FAILED,
-   TEGRADRM_STREAM_READY,
+enum grate_stream_status {
+   GRATE_STREAM_FREE,
+   GRATE_STREAM_CONSTRUCT,
+   GRATE_STREAM_CONSTRUCTION_FAILED,
+   GRATE_STREAM_READY,
 };
 
-struct tegra_command_buffer {
+struct grate_command_buffer {
    struct drm_tegra_pushbuf *pushbuf;
 };
 
-struct tegra_stream {
-   enum tegra_stream_status status;
+struct grate_stream {
+   enum grate_stream_status status;
 
    struct drm_tegra_job *job;
    struct drm_tegra_channel *channel;
 
-   struct tegra_command_buffer buffer;
+   struct grate_command_buffer buffer;
    int num_words;
    uint32_t class_id;
 };
 
-struct tegra_reloc {
+struct grate_reloc {
    const void *addr;
    struct drm_tegra_bo *bo;
    uint32_t offset;
@@ -64,39 +64,39 @@ struct tegra_reloc {
 
 /* Stream operations */
 int
-tegra_stream_create(struct drm_tegra *drm,
+grate_stream_create(struct drm_tegra *drm,
                     struct drm_tegra_channel *channel,
-                    struct tegra_stream *stream,
+                    struct grate_stream *stream,
                     uint32_t words_num);
 void
-tegra_stream_destroy(struct tegra_stream *stream);
+grate_stream_destroy(struct grate_stream *stream);
 
 int
-tegra_stream_begin(struct tegra_stream *stream);
+grate_stream_begin(struct grate_stream *stream);
 
 int
-tegra_stream_end(struct tegra_stream *stream);
+grate_stream_end(struct grate_stream *stream);
 
 int
-tegra_stream_flush(struct tegra_stream *stream);
+grate_stream_flush(struct grate_stream *stream);
 
 int
-tegra_stream_push(struct tegra_stream *stream, uint32_t word);
+grate_stream_push(struct grate_stream *stream, uint32_t word);
 
 int
-tegra_stream_push_setclass(struct tegra_stream *stream,
+grate_stream_push_setclass(struct grate_stream *stream,
                            enum host1x_class class_id);
 
 int
-tegra_stream_push_reloc(struct tegra_stream *stream,
+grate_stream_push_reloc(struct grate_stream *stream,
                         struct drm_tegra_bo *bo, unsigned offset);
 
-struct tegra_reloc
-tegra_reloc(const void *var_ptr, struct drm_tegra_bo *bo,
+struct grate_reloc
+grate_reloc(const void *var_ptr, struct drm_tegra_bo *bo,
             uint32_t offset, uint32_t var_offset);
 
 int
-tegra_stream_push_words(struct tegra_stream *stream, const void *addr,
+grate_stream_push_words(struct grate_stream *stream, const void *addr,
                         unsigned words, int num_relocs, ...);
 
 #endif
