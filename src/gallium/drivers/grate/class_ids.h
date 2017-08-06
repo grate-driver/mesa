dc1f30a6ac062ff820413e60b52e4650b01871c6
Erik Faye-Lund <kusmabite@gmail.com>
2017-08-06 23:53:41 +0200

    grate: introduce tegra_stream
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

---
+++ src/gallium/drivers/grate/class_ids.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Dmitry Osipenko <digetx@gmail.com>
+ * Copyright (C) 2012-2013 NVIDIA Corporation.
+ * SPDX-License-Identifier: MIT
+ * Authors:
+ *	Arto Merilainen <amerilainen@nvidia.com>
+ */
+
+#ifndef CLASS_IDS_H_
+#define CLASS_IDS_H_
+
+enum host1x_class {
+        HOST1X_CLASS_GR2D    = 0x51,
+        HOST1X_CLASS_GR2D_SB = 0x52,
+        HOST1X_CLASS_GR3D    = 0x60,
+};
+
+#endif
