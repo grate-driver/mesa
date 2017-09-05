8f15b6e7b22faa143100de9ae1733cbfe40e0c5f
Erik Faye-Lund <kusmabite@gmail.com>
2017-09-05 13:53:30 +0200

    HACK: grate: transpile TGSI to FP

--- src/gallium/drivers/grate/grate_compiler.h
+++ src/gallium/drivers/grate/grate_compiler.h
@@ -12,7 +12,16 @@ struct grate_vpe_shader {
    uint16_t output_mask;
 };
 
+struct grate_fp_shader {
+   struct list_head fp_instructions;
+   struct list_head alu_instructions;
+   struct list_head mfu_instructions;
+};
+
 void
 grate_tgsi_to_vpe(struct grate_vpe_shader *vpe, struct tgsi_parse_context *tgsi);
+
+void
+grate_tgsi_to_fp(struct grate_fp_shader *fp, struct tgsi_parse_context *tgsi);
 
 #endif
