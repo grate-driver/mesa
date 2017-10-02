74f19032341748ad257a0c2527624b661e2d2e6f
Jason Ekstrand <jason.ekstrand@intel.com>
2017-10-02 09:53:50 -0700

    anv/pipeline: Call nir_lower_system_valaues after brw_preprocess_nir
    We currently have a bug where nir_lower_system_values gets called before
    nir_lower_var_copies so it will miss any system value uses which come
    from a copy_var intrinsic.  Moving it to after brw_preprocess_nir fixes
    this problem.
    
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable@lists.freedesktop.org
    (cherry picked from commit 279f8fb69cf68d05287e14f60cf67fc025643bc4)
    [Andres Gomez: resolve trivial conflicts]
    Signed-off-by: Andres Gomez <agomez@igalia.com>
    
    Conflicts:
    	src/intel/vulkan/anv_pipeline.c

--- src/intel/vulkan/anv_pipeline.c
+++ src/intel/vulkan/anv_pipeline.c
@@ -171,12 +171,13 @@ anv_shader_compile_to_nir(struct anv_pipeline *pipeline,
    NIR_PASS_V(nir, nir_propagate_invariant);
    NIR_PASS_V(nir, nir_lower_io_to_temporaries,
               entry_point->impl, true, false);
-   NIR_PASS_V(nir, nir_lower_system_values);
 
    /* Vulkan uses the separate-shader linking model */
    nir->info.separate_shader = true;
 
    nir = brw_preprocess_nir(compiler, nir);
+
+   NIR_PASS_V(nir, nir_lower_system_values);
 
    NIR_PASS_V(nir, nir_lower_clip_cull_distance_arrays);
 
