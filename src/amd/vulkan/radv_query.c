61c64f64d73096acb9ee352ac9535ca7e1a930e8
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2018-11-05 09:54:28 +0100

    radv: disable conditional rendering for vkCmdCopyQueryPoolResults()
    VK_EXT_conditional_rendering says that copy commands should not be
    affected by conditional rendering.
    
    Cc: 18.2 18.3 <mesa-stable@lists.freedesktop.org>
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 0a0aa2ba6c37085948cc7efe11695ebe4fdf12fe)
    
    Conflicts:
    	src/amd/vulkan/radv_query.c

--- src/amd/vulkan/radv_query.c
+++ src/amd/vulkan/radv_query.c
@@ -913,6 +913,7 @@ static void radv_query_shader(struct radv_cmd_buffer *cmd_buffer,
 {
 	struct radv_device *device = cmd_buffer->device;
 	struct radv_meta_saved_state saved_state;
+	bool old_predicating;
 
 	if (!*pipeline) {
 		VkResult ret = radv_device_init_meta_query_state_internal(device);
@@ -926,6 +927,12 @@ static void radv_query_shader(struct radv_cmd_buffer *cmd_buffer,
 		       RADV_META_SAVE_COMPUTE_PIPELINE |
 		       RADV_META_SAVE_CONSTANTS |
 		       RADV_META_SAVE_DESCRIPTORS);
+
+	/* VK_EXT_conditional_rendering says that copy commands should not be
+	 * affected by conditional rendering.
+	 */
+	old_predicating = cmd_buffer->state.predicating;
+	cmd_buffer->state.predicating = false;
 
 	struct radv_buffer dst_buffer = {
 		.bo = dst_bo,
@@ -1008,6 +1015,8 @@ static void radv_query_shader(struct radv_cmd_buffer *cmd_buffer,
 	cmd_buffer->state.flush_bits |= RADV_CMD_FLAG_INV_GLOBAL_L2 |
 	                                RADV_CMD_FLAG_INV_VMEM_L1 |
 	                                RADV_CMD_FLAG_CS_PARTIAL_FLUSH;
+	/* Restore conditional rendering. */
+	cmd_buffer->state.predicating = old_predicating;
 
 	radv_meta_restore(&saved_state, cmd_buffer);
 }
