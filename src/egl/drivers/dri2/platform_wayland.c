555a629b357669b492653dfba6cfd07e2c67cbf0
Nicolai Hähnle <nicolai.haehnle@amd.com>
2018-11-28 18:30:36 +0100

    egl/wayland: rather obvious build fix
    Fixes: ce74a7bb8de7 ("egl/wayland: plug memory leak in drm_handle_device()")
    Fixes: c59d3aa4b9bc ("egl/wayland: bail out when drmGetMagic fails")
    (cherry picked from commit c02390f8fcd367c7350db568feabb2f062efca14)

--- src/egl/drivers/dri2/platform_wayland.c
+++ src/egl/drivers/dri2/platform_wayland.c
@@ -1129,7 +1129,7 @@ drm_handle_device(void *data, struct wl_drm *drm, const char *device)
       _eglLog(_EGL_WARNING, "wayland-egl: could not open %s (%s)",
               dri2_dpy->device_name, strerror(errno));
       free(dri2_dpy->device_name);
-      dri2_dpy->device_name = NULL:
+      dri2_dpy->device_name = NULL;
       return;
    }
 
@@ -1140,7 +1140,7 @@ drm_handle_device(void *data, struct wl_drm *drm, const char *device)
          close(dri2_dpy->fd);
          dri2_dpy->fd = -1;
          free(dri2_dpy->device_name);
-         dri2_dpy->device_name = NULL:
+         dri2_dpy->device_name = NULL;
          _eglLog(_EGL_WARNING, "wayland-egl: drmGetMagic failed");
          return;
       }
