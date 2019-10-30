b5b09acb748ea4666c4587856af65fbe319f5b49
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2019-10-30 14:28:01 +0100

    radeonsi: tell the shader disk cache what IR is used
    Until 8bef4df196fbb the IR (TGSI or NIR) was used in disk_cache driver_flags.
    This commit restores this features to avoid crashing when switching from
    one IR to the other.
    
    As radeonsi's default is TGSI, I used "driver_flags & 0x8000000 = 0" for TGSI
    to keep the same driver_flags.
    
    Fixes: 8bef4df196f ("radeonsi: add si_debug_options for convenient adding/removing of options")
    
    Reviewed-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>

--- src/gallium/drivers/radeonsi/si_pipe.c
+++ src/gallium/drivers/radeonsi/si_pipe.c
@@ -877,6 +877,9 @@ static void si_disk_cache_create(struct si_screen *sscreen)
 	/* These flags affect shader compilation. */
 	#define ALL_FLAGS (DBG(SI_SCHED) | DBG(GISEL))
 	uint64_t shader_debug_flags = sscreen->debug_flags & ALL_FLAGS;
+	/* Reserve left-most bit for tgsi/nir selector */
+	assert(!(shader_debug_flags & (1 << 31)));
+	shader_debug_flags |= ((sscreen->options.enable_nir & 0x1) << 31);
 
 	/* Add the high bits of 32-bit addresses, which affects
 	 * how 32-bit addresses are expanded to 64 bits.
@@ -997,6 +1000,13 @@ radeonsi_screen_create_impl(struct radeon_winsys *ws,
 	if (!si_init_shader_cache(sscreen)) {
 		FREE(sscreen);
 		return NULL;
+	}
+
+	{
+#define OPT_BOOL(name, dflt, description) \
+		sscreen->options.name = \
+			driQueryOptionb(config->options, "radeonsi_"#name);
+#include "si_debug_options.h"
 	}
 
 	si_disk_cache_create(sscreen);
@@ -1124,13 +1134,6 @@ radeonsi_screen_create_impl(struct radeon_winsys *ws,
 		driQueryOptionb(config->options, "radeonsi_assume_no_z_fights");
 	sscreen->commutative_blend_add =
 		driQueryOptionb(config->options, "radeonsi_commutative_blend_add");
-
-	{
-#define OPT_BOOL(name, dflt, description) \
-		sscreen->options.name = \
-			driQueryOptionb(config->options, "radeonsi_"#name);
-#include "si_debug_options.h"
-	}
 
 	sscreen->has_gfx9_scissor_bug = sscreen->info.family == CHIP_VEGA10 ||
 					sscreen->info.family == CHIP_RAVEN;
