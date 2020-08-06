204b736d1900e9a1338bb9e3bcde47d00d5a6784
Eric Engestrom <eric@engestrom.ch>
2020-08-06 16:05:51 +0200

    driconf: fix force_gl_vendor description
    The option is not a toggle to "allow GPU vendor to be overridden", it
    *is* the override.
    
    Fixes: dca119f12c291d7665d7 ("mesa/gallium: add dric option to allow overriding GL vendor string")
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6207>
    (cherry picked from commit 7fbadfc385c359fd291d58a75fbe6ce3fdc91747)

--- src/util/xmlpool/t_options.h
+++ src/util/xmlpool/t_options.h
@@ -152,7 +152,7 @@ DRI_CONF_OPT_END
 
 #define DRI_CONF_FORCE_GL_VENDOR(def) \
 DRI_CONF_OPT_BEGIN(force_gl_vendor, string, def) \
-        DRI_CONF_DESC(en,gettext("Allow GPU vendor to be overridden.")) \
+        DRI_CONF_DESC(en,gettext("Override GPU vendor string.")) \
 DRI_CONF_OPT_END
 
 #define DRI_CONF_FORCE_COMPAT_PROFILE(def) \
