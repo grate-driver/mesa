a2f1b6d268bc2afca357a6bf3c9aebc763840f46
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2020-08-25 09:39:06 -0400

    zink: reorder create_stream_output_target to fix failure case leak
    the previous version of this leaked a reference to the streamout buffer here
    
    thanks to deltragon on my blog for pointing this out!
    
    Fixes: 37778fcd9a35 ("zink: implement transform feedback support to finish off opengl 3.0")
    
    Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/6457>
    (cherry picked from commit 7971918924dd84c2f33a5b7f0a8bf0c23ed994d8)

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -1048,12 +1048,6 @@ zink_create_stream_output_target(struct pipe_context *pctx,
    if (!t)
       return NULL;
 
-   t->base.reference.count = 1;
-   t->base.context = pctx;
-   pipe_resource_reference(&t->base.buffer, pres);
-   t->base.buffer_offset = buffer_offset;
-   t->base.buffer_size = buffer_size;
-
    /* using PIPE_BIND_CUSTOM here lets us create a custom pipe buffer resource,
     * which allows us to differentiate and use VK_BUFFER_USAGE_TRANSFORM_FEEDBACK_COUNTER_BUFFER_BIT_EXT
     * as we must for this case
@@ -1063,6 +1057,12 @@ zink_create_stream_output_target(struct pipe_context *pctx,
       FREE(t);
       return NULL;
    }
+
+   t->base.reference.count = 1;
+   t->base.context = pctx;
+   pipe_resource_reference(&t->base.buffer, pres);
+   t->base.buffer_offset = buffer_offset;
+   t->base.buffer_size = buffer_size;
 
    return &t->base;
 }
