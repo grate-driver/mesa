f7e7bea4903978fe40914e0feb386715153734c2
Italo Nicola <italonicola@collabora.com>
2021-05-11 10:29:01 +0000

    panfrost: fix GL_EXT_multisampled_render_to_texture regression
    Signed-off-by: Italo Nicola <italonicola@collabora.com>
    Fixes: ff3eada7eb4 ("panfrost: Use the generic preload and FB helpers in the gallium driver")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10962>
    (cherry picked from commit c746747cb82f10fb5f1f3b625e5833d8d2c1e042)

--- src/gallium/drivers/panfrost/pan_job.c
+++ src/gallium/drivers/panfrost/pan_job.c
@@ -779,6 +779,7 @@ panfrost_batch_to_fb_info(const struct panfrost_batch *batch,
                 rts[i].first_layer = surf->u.tex.first_layer;
                 rts[i].last_layer = surf->u.tex.last_layer;
                 rts[i].image = &prsrc->image;
+                rts[i].nr_samples = surf->nr_samples ? : MAX2(surf->texture->nr_samples, 1);
                 memcpy(rts[i].swizzle, id_swz, sizeof(rts[i].swizzle));
                 fb->rts[i].state = &prsrc->state;
                 fb->rts[i].view = &rts[i];
@@ -806,6 +807,7 @@ panfrost_batch_to_fb_info(const struct panfrost_batch *batch,
                 zs->first_layer = surf->u.tex.first_layer;
                 zs->last_layer = surf->u.tex.last_layer;
                 zs->image = &prsrc->image;
+                zs->nr_samples = surf->nr_samples ? : MAX2(surf->texture->nr_samples, 1);
                 memcpy(zs->swizzle, id_swz, sizeof(zs->swizzle));
                 fb->zs.view.zs = zs;
                 fb->zs.state.zs = &prsrc->state;
@@ -823,6 +825,7 @@ panfrost_batch_to_fb_info(const struct panfrost_batch *batch,
                         s->first_layer = surf->u.tex.first_layer;
                         s->last_layer = surf->u.tex.last_layer;
                         s->image = &prsrc->separate_stencil->image;
+                        s->nr_samples = surf->nr_samples ? : MAX2(surf->texture->nr_samples, 1);
                         memcpy(s->swizzle, id_swz, sizeof(s->swizzle));
                         fb->zs.view.s = s;
                         fb->zs.state.s = &prsrc->separate_stencil->state;
