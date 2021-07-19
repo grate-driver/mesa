a7199a03abaed4a30eed03694b6abd590a399740
Dave Airlie <airlied@redhat.com>
2021-07-19 11:57:12 +1000

    llvmpipe: add support for time elapsed queries.
    It turns out for QBO you really need to explicitly support time
    elapsed queries to avoid wierd interactions with the non-qbo
    query paths.
    
    Fixes: 506e51b8560f ("llvmpipe: initial query buffer object support. (v2)")
    Reviewed-by: Michel Dänzer <mdaenzer@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11946>
    (cherry picked from commit 2f5cd08ede0b9d445535d54beffcad0e5c4a62bb)

--- src/gallium/drivers/llvmpipe/lp_setup.c
+++ src/gallium/drivers/llvmpipe/lp_setup.c
@@ -1532,7 +1532,8 @@ lp_setup_begin_query(struct lp_setup_context *setup,
    if (!(pq->type == PIPE_QUERY_OCCLUSION_COUNTER ||
          pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
          pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
-         pq->type == PIPE_QUERY_PIPELINE_STATISTICS))
+         pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
+         pq->type == PIPE_QUERY_TIME_ELAPSED))
       return;
 
    /* init the query to its beginning state */
@@ -1584,7 +1585,8 @@ lp_setup_end_query(struct lp_setup_context *setup, struct llvmpipe_query *pq)
           pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
           pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
           pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
-          pq->type == PIPE_QUERY_TIMESTAMP) {
+          pq->type == PIPE_QUERY_TIMESTAMP ||
+          pq->type == PIPE_QUERY_TIME_ELAPSED) {
          if (pq->type == PIPE_QUERY_TIMESTAMP &&
                !(setup->scene->tiles_x | setup->scene->tiles_y)) {
             /*
@@ -1620,7 +1622,8 @@ fail:
    if (pq->type == PIPE_QUERY_OCCLUSION_COUNTER ||
       pq->type == PIPE_QUERY_OCCLUSION_PREDICATE ||
       pq->type == PIPE_QUERY_OCCLUSION_PREDICATE_CONSERVATIVE ||
-      pq->type == PIPE_QUERY_PIPELINE_STATISTICS) {
+      pq->type == PIPE_QUERY_PIPELINE_STATISTICS ||
+      pq->type == PIPE_QUERY_TIME_ELAPSED) {
       unsigned i;
 
       /* remove from active binned query list */
