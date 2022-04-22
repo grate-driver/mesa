001a5c73cc494dc359236c854ee202f0725e22a2
Boris Brezillon <boris.brezillon@collabora.com>
2022-04-22 10:55:55 -0700

    dzn: Add missing VKAPI_{ATTR,CALL} specifiers to BeginCommandBuffer()
    Fixes: a012b219640 ("microsoft: Initial vulkan-on-12 driver")
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16109>
    (cherry picked from commit cb9ad71d75f0b14bfbf33a61539f474322edf165)

--- src/microsoft/vulkan/dzn_cmd_buffer.cpp
+++ src/microsoft/vulkan/dzn_cmd_buffer.cpp
@@ -305,7 +305,7 @@ dzn_ResetCommandBuffer(VkCommandBuffer commandBuffer,
    return dzn_cmd_buffer_reset(cmdbuf);
 }
 
-VkResult
+VKAPI_ATTR VkResult VKAPI_CALL
 dzn_BeginCommandBuffer(VkCommandBuffer commandBuffer,
                        const VkCommandBufferBeginInfo *info)
 {
