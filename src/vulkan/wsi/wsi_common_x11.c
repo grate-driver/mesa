b78272daa59a47f052c31e123b256843cfde29e4
Georg Lehmann <dadschoorse@gmail.com>
2022-05-08 01:30:42 +0200

    wsi/x11: Don't leak xcb_get_geometry_reply_t.
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/6443
    Fixes: 44a20baeb8b ("wsi/x11: Avoid using xcb_wait_for_special_event in FIFO modes")
    Signed-off-by: Georg Lehmann <dadschoorse@gmail.com>
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16384>

--- src/vulkan/wsi/wsi_common_x11.c
+++ src/vulkan/wsi/wsi_common_x11.c
@@ -1495,6 +1495,7 @@ x11_manage_fifo_queues(void *state)
                result = VK_ERROR_SURFACE_LOST_KHR;
                goto fail;
             }
+            free(geometry);
          }
       }
    }
