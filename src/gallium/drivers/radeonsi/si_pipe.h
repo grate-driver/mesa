ebda38196a15e49ae3bfef2e34684a4da126da49
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2022-06-14 16:35:49 +0200

    radeonsi: add helper to use si_screen::aux_context
    This context needs to be locked before usage, and flushed after.
    If it's forgotten, radeonsi may crash (eg #6666).
    
    To avoid this kind of error, introduce 2 helpers.
    
    cc: mesa-stable
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/17032>
    (cherry picked from commit bda1c081bd6e9e4ec144f963c8899cd0d17b211f)

--- src/gallium/drivers/radeonsi/si_pipe.h
+++ src/gallium/drivers/radeonsi/si_pipe.h
@@ -1504,6 +1504,8 @@ void si_init_compute_functions(struct si_context *sctx);
 /* si_pipe.c */
 void si_init_compiler(struct si_screen *sscreen, struct ac_llvm_compiler *compiler);
 void si_init_aux_async_compute_ctx(struct si_screen *sscreen);
+struct si_context* si_get_aux_context(struct si_screen *sscreen);
+void si_put_aux_context_flush(struct si_screen *sscreen);
 
 /* si_perfcounters.c */
 void si_init_perfcounters(struct si_screen *screen);
