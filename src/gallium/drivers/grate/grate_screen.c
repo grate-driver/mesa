1f72474b687c400dc29f1fd571a4504484024da4
Nicolas Chauvet <kwizart@gmail.com>
2022-12-01 10:34:40 +0100

    Add missing vertex-shader caps
    Signed-off-by: Nicolas Chauvet <kwizart@gmail.com>

--- src/gallium/drivers/grate/grate_screen.c
+++ src/gallium/drivers/grate/grate_screen.c
@@ -537,8 +537,10 @@ grate_screen_get_shader_param(struct pipe_screen *pscreen,
           return 0;
 
       case PIPE_SHADER_CAP_FP16:
+      case PIPE_SHADER_CAP_FP16_CONST_BUFFERS:
       case PIPE_SHADER_CAP_FP16_DERIVATIVES:
       case PIPE_SHADER_CAP_INT16:
+      case PIPE_SHADER_CAP_INT64_ATOMICS:
       case PIPE_SHADER_CAP_GLSL_16BIT_CONSTS:
           return 0;
 
